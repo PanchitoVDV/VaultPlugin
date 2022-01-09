0c4abac0aa43645b934d2ee81c33b10069fbf3a4
panchito <panchito.vandevelde04@gmail.com>
2022-01-09 12:52:06 +0100

    Register command "vault"

--- src/main/java/me/panchito/vaultplugin/VaultPlugin.java
+++ src/main/java/me/panchito/vaultplugin/VaultPlugin.java
@@ -1,5 +1,6 @@
 package me.panchito.vaultplugin;
 
+import commands.VaultCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class VaultPlugin extends JavaPlugin {
@@ -8,6 +9,9 @@ public final class VaultPlugin extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         System.out.printf("Vault plugin has started up!");
+
+        // Register the Vault command
+        getCommand("vault").setExecutor(new VaultCommand());
 
     }
 
