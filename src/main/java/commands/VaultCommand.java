46f31be490a319c061286166afde9fa5cc85c0d2
panchito <panchito.vandevelde04@gmail.com>
2022-01-09 14:02:42 +0100

    Reworked the whole vault command

--- src/main/java/commands/VaultCommand.java
+++ src/main/java/commands/VaultCommand.java
@@ -1,54 +1,60 @@
 package commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
 
 public class VaultCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (sender instanceof Player) {
+        if (sender instanceof Player){
             Player player = (Player) sender;
-            if (player.hasPermission("vault.open")) {
+            //Creating inventories. 1: Name of player inventory is linked to 2: How many slots in the inventory 3: Title of inventory
+            Inventory vault = Bukkit.createInventory(player, 9, "Your Vault");
 
-                Inventory vault = Bukkit.createInventory(player, 9, "Your private vault");
-                player.openInventory(vault);
+            //Put items in the inventory https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/Inventory.html
+            ItemStack item1 = new ItemStack(Material.COOKED_BEEF, 16); //16 Beef
+            vault.setItem(3, item1); //Puts the item in a specific item slot
+            vault.addItem(item1); //Adds the item to the next available spot randomly
+            vault.clear(); //Empties the inventory
 
-                Material type;
-                ItemStack diamond = new ItemStack(Material.DIAMOND);
+            //A way to store multiple items
+            ItemStack item2 = new ItemStack(Material.ACACIA_FENCE);
 
-                vault.setItem(4, diamond);
-
-                vault.addItem(diamond);
-
-                vault.clear();
-                //
-
-                ItemStack beef = new ItemStack(Material.COOKED_BEEF);
-                ItemStack[] items = {diamond, beef};
-
-                vault.setContents(items);
-
-
-                player.openInventory(vault);
-
-
-                player.openInventory(vault);
+            //More on items
+            item2.addUnsafeEnchantment(Enchantment.DURABILITY, 100); // Add enchantments
+            ItemMeta meta = item2.getItemMeta(); //get the metadata of the item.
+            meta.setDisplayName(ChatColor.AQUA + "Item name");
+            ArrayList<String> lore = new ArrayList<>(); //Item lore: the stuff showed under the item title
+            lore.add(ChatColor.GOLD + "some lore");
+            lore.add(ChatColor.DARK_PURPLE + "some more lore heehehehe");
+            meta.setLore(lore); //Add the lore to the item
+            item2.setItemMeta(meta); //set all the metadata for the item
 
 
-            }
-        } else {
-            System.out.printf("You have to be a player to run this command!");
+            ItemStack[] items = {item1, item2};
+            vault.setContents(items); //Puts the item array into the inventory, replaces everything else
+            //
+
+            //You can get the contents of the inventory and save it
+            ItemStack[] items_from_inventory = vault.getContents();
+
+            player.openInventory(vault);
         }
-
 
         return true;
     }
+
 }
