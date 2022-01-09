332604ee4f5d986dc754e7656c072a40e09ad693
panchito <panchito.vandevelde04@gmail.com>
2022-01-09 12:40:16 +0100

    Setup VaultPlugin

---
+++ src/main/java/me/panchito/vaultplugin/VaultPlugin.java
@@ -0,0 +1,13 @@
+package me.panchito.vaultplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VaultPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+}
